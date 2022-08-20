47bed4687cea12a0cbe01a5dbfb5a76c37936635
klei22 <klei22>
2022-08-20 00:43:18 -0700

    Add file reading capability

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::{App, Arg, ArgMatches};
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn get_args() -> ArgMatches {
     App::new("cli_template")
         .version("0.1.0")
@@ -11,25 +15,35 @@ fn get_args() -> ArgMatches {
                 .long("input")
                 .value_name("INPUT_FILE")
                 .help("path to input file")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
-            Arg::with_name("quiet_flag")
-                .short('q')
-                .help("quiet flag, do not emit output")
+            Arg::with_name("all_caps")
+                .short('A')
+                .help("set for all caps")
                 .takes_value(false),
         )
         .get_matches()
 }
 
+fn print_file_contents(filename: &str, all_caps: &bool) {
+    let f = File::open(filename).unwrap();
+    let reader = BufReader::new(f);
+    for buf_line in reader.lines() {
+        let line = buf_line.unwrap();
+        match all_caps {
+            true => println!("{}", line.to_uppercase()),
+            false => println!("{}", line),
+        }
+    }
+}
+
 fn main() {
     let matches = get_args();
 
-    let quiet = matches.is_present("quiet_flag");
+    let all_caps = matches.is_present("all_caps");
 
-    if !quiet {
-        if let Some(i) = matches.value_of("input_file") {
-            println!("Value for input: {}", i);
-        }
+    if let Some(filename) = matches.value_of("input_file") {
+        print_file_contents(&filename, &all_caps);
     }
 }
